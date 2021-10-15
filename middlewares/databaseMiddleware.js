81999506226a9f77d4491e639d0ca22db374a3d6
dev2820 <second28200@gmail.com>
2021-10-15 22:58:53 +0900

    nginx 디버깅중2

--- middlewares/databaseMiddleware.js
+++ middlewares/databaseMiddleware.js
@@ -16,15 +16,15 @@ const registConnection = async (req,res,next)=>{
     }
     catch(err) {
         console.log(err);
-        console.log({
+        res.status(500).json({ message: "db connection error",
+        env:{
             host:process.env.HOST,
             user:process.env.USER,
             password:process.env.PASSWORD,
             database:process.env.DATABASE,
             port: process.env.DB_PORT || 3306,
             connectionLimit: process.env.LIMIT || 10
-        })
-        res.status(500).json({ message: "db connection error"})
+        }})
     }
 }
 
