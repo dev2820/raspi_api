feb664c74c1948d16ea1ae7f932aac7a1c52b078
dev2820 <second28200@gmail.com>
2021-10-15 22:56:56 +0900

    nginx 디버깅중

--- middlewares/databaseMiddleware.js
+++ middlewares/databaseMiddleware.js
@@ -8,14 +8,7 @@ const pool = createPool({
         port: process.env.DB_PORT || 3306,
         connectionLimit: process.env.LIMIT || 10
 });
-console.log({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    database:process.env.DATABASE,
-    port: process.env.DB_PORT || 3306,
-    connectionLimit: process.env.LIMIT || 10
-})
+
 const registConnection = async (req,res,next)=>{
     try {
         res.locals.conn = await pool.getConnection(async conn => conn);
@@ -23,6 +16,14 @@ const registConnection = async (req,res,next)=>{
     }
     catch(err) {
         console.log(err);
+        console.log({
+            host:process.env.HOST,
+            user:process.env.USER,
+            password:process.env.PASSWORD,
+            database:process.env.DATABASE,
+            port: process.env.DB_PORT || 3306,
+            connectionLimit: process.env.LIMIT || 10
+        })
         res.status(500).json({ message: "db connection error"})
     }
 }
