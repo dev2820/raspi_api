ffff3eb927362203b5bad9efb7a0fc399f22cfcc
dev2820 <second28200@gmail.com>
2021-10-15 22:56:05 +0900

    debug 코드 추가

--- middlewares/databaseMiddleware.js
+++ middlewares/databaseMiddleware.js
@@ -8,7 +8,14 @@ const pool = createPool({
         port: process.env.DB_PORT || 3306,
         connectionLimit: process.env.LIMIT || 10
 });
-
+console.log({
+    host:process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE,
+    port: process.env.DB_PORT || 3306,
+    connectionLimit: process.env.LIMIT || 10
+})
 const registConnection = async (req,res,next)=>{
     try {
         res.locals.conn = await pool.getConnection(async conn => conn);
