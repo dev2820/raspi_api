58918078b93901411a29ae5f8f02abcfa58f5761
dev2820 <second28200@gmail.com>
2021-10-15 23:23:51 +0900

    debug 코드 지움

--- app.js
+++ app.js
@@ -48,8 +48,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.json({
-    'message': err.message,
-    'debug':{ env:process.env }
+    'message': err.message
   });
 });
 
